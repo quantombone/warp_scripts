23fe13a957cfb4e55f4b7a2ff47cf89a59146bee
Tomasz J Malisiewicz <tomasz@asia.csail.mit.edu>
2011-11-02 22:42:05 -0400

    fixing for the mit peoples to rally and win it

--- sc.sh
+++ sc.sh
@@ -2,8 +2,9 @@
 
 # A shell script to start the main screen session on ANIM machines
 # This can in theory run also on WARP (but please please only use PBS not this!)
+# This works on the vision machines at CSAIL now
 # hacked up by Tomasz Malisiewicz (tomasz@cmu.edu)
-# Run like: ./sc.sh script_name 8 10
+# Run like: ./sc.sh script_name
 # The above command will run "script_name" on machine 8 through machine 10
 # $Id: sc.sh,v 1.20 2009/03/03 05:33:07 tmalisie Exp $
 # $Date: 2009/03/03 05:33:07 $
@@ -15,7 +16,7 @@
 #read command line argument 1 which is ALWAYS required
 if [ ! -n "$1" ]
     then
-    echo "Usage: " $0 " script_to_run [start_machine_id] [end_machine_id]"
+    echo "Usage: " $0 " script_to_run"
     echo "      script_to_run is MANDTORY"
     exit
 else
@@ -36,49 +37,24 @@ if [ $ALREADY_RUNNING = "1" ]; then
     exit;
 fi
 
-echo 'Clearing Out Log Files'
-rm /afs/cs.cmu.edu/user/${USER}/www/screenlogs/mylog*
+#echo 'Clearing Out Log Files'
+#rm /afs/cs.cmu.edu/user/${USER}/www/screenlogs/mylog*
 
 ## START THE DRIVER SCREEN SESSION ###
 screen -L -c animrc  
 
-#set the default start machine and end machine
-export START_MACHINE=10
-export END_MACHINE=15
-
-#read optional command line argument 2
-if [ -n "$2" ]
-    then
-    export START_MACHINE=$2
-    echo "Setting START_MACHINE=" $2
-fi
-
-#read optional command line argument 2
-if [ -n "$3" ]
-    then
-    export END_MACHINE=$3
-    echo "Setting END_MACHINE=" $3
-fi
-
-#Here we must use a temporary HOME environment since we are skipping
-#kerb and don't have local directory write access which compiled
-#matlab scripts require
-#STARTDIR=/nfs/hn01/tmalisie/compiled_labelme/
-#export PROCESS_STRING="(setenv HOME /nfs/hn01/tmalisie/temphome/; cd ${STARTDIR}; nice +10 ./${PROC})"
-
 #here we run uncompiled matlabs
-STARTDIR=/nfs/hn22/tmalisie/ddip/
-export PROCESS_STRING="(setenv HOME /nfs/hn01/tmalisie/temphome/; cd ${STARTDIR}; nice matlab -nodesktop -nosplash -r \"addpath(genpath(pwd));try,${PROC};,catch,disp('Error with script ${PROC}');end;exit(1);\")"
+export PROCESS_STRING="(/afs/csail.mit.edu/system/amd64_linux26/matlab/latest/bin/matlab -nodesktop -nosplash -r \"try,${PROC};,catch,disp('Error with script');end;exit(1);\")"
 
 #STARTID is a temporary counter variable
 STARTID='1'
 
-#create a machine list which is simply START:END
-MACHINELIST=`echo \`seq $START_MACHINE $END_MACHINE\``
+#run machine list
+. ./machine_list.sh
 
 for ind in $MACHINELIST
 do
-  export SSH_STRING="ssh anim${ind}.graphics.cs.cmu.edu"
+  export SSH_STRING="ssh ${ind}"
 
   #echo Initializing Virtual Terminal $ind
   # a short sleep is critical to let screen re-adjust its socks
